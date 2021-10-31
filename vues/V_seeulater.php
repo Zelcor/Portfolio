8655bf717ed256e0482b6bf6013ddd2172aef63d
Clément <clementloze@hotmail.fr>
2021-10-31 14:17:19 +0100

    changement message annoncent le passage php (mise en ligne)

--- vues/V_seeulater.php
+++ vues/V_seeulater.php
@@ -2,7 +2,7 @@
             <div class="cadreCV">
             <div class="css/portfolio.css">
                 <img src="../image/en%20travaux.jpg" class="rounded mx-auto d-block">
-                <h1 class="d-flex justify-content-center"> Ce site n'est pas fini, en effet je suis entrain de passer en PHP (je travaille dur dessus) !</h1>
+                <h1 class="d-flex justify-content-center"> Ce site n'est pas fini, mais il vient de passer en php !! son dévelloppement va être bien plus rapide tout de suite :o</h1>
             </div>
             </div>
         </div> <!-- Emplacement pour le contenu principal de la page -->
