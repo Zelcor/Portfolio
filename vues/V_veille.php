81bbeee81f6ee54fa68bf6b17e247edebdcbf2dd
Clément <clementloze@hotmail.fr>
2021-10-31 19:06:18 +0100

    Ajout de la page expliquant ma veille

---
+++ vues/V_veille.php
@@ -0,0 +1,70 @@
+    <div class="jumbotron" id="jumbotron">
+        <div class="row center">
+            <section class="jumbotron text-center">
+                <div class="container">
+                    <h1 class="jumbotron-heading">Historique de veille technologique</h1>
+                    <p class="lead text-muted">
+                        Vous allez pouvoir trouver ci-après un graphique représentant ma veille technologique :
+                    </p>
+                </div>
+                <!-- Timeline -->
+                    <div class="card-body">
+                        <div class="mt-5">
+                            <div class="timeline">
+                                <div class="timeline-wrapper timeline-inverted timeline-wrapper-cyber">
+                                    <div class="timeline-badge"></div>
+                                    <div class="timeline-panel">
+                                        <div class="timeline-heading">
+                                            <h6 class="timeline-title">Tendance Cybersécurité 2022</h6>
+                                        </div>
+                                        <div class="timeline-body">
+                                            <p>Tout d’abord on nous apprend qu’2020, près de la moitié des entreprises françaises ont subi une cyberattaque et parmi celles qui ont payé la rançon 1/4 sont des entreprises appartenant au secteur de la technologie, des médias ou de la communication. Ensuite on demande à Sabrina Feddal ingénieure et consultante cybersécurité quels sont enjeux pour 2022 et elle nous fait part que l’un des plus gros enjeux est de perfectionner les outils de machine learning. Elle a ensuite abordé une arnaque j’avais déjà entendu parler par le Youtubeur sandoz : l’arnaque à la cryptomonnaie.</p>
+                                        </div>
+                                        <div class="timeline-footer d-flex align-items-center flex-wrap">
+                                            <i class="mdi mdi-heart-outline text-muted mr-1"></i>
+                                            <span><a href="https://www.kaspersky.fr/resource-center/definitions/cryptocurrency-scams">Source</a></span>
+                                            <span class="ml-md-auto font-weight-bold">Octobre 2021</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="timeline-wrapper timeline-wrapper-ethique">
+                                    <div class="timeline-badge"></div>
+                                    <div class="timeline-panel">
+                                        <div class="timeline-heading">
+                                            <h6 class="timeline-title">Reportage the social dilema</h6>
+                                        </div>
+                                        <div class="timeline-body">
+                                            <p>Ayant toujours eu de l’intérêt sur les dérives technologiques. On m’a recommander de regarder ce reportage qui m’a permis d’en savoir plus sur la dérive algorithmique actuelle. Suite à ce reportage je suis aller me renseigner sur leur <a href="https://www.thesocialdilemma.com/">site officiel</a> Ce qui m’a permit de découvrir : <a href="https://www.humanetech.com/take-control">Center for Humane Technology</a>, et d’appliquer leurs conseils sur mon utilisation du monde numérique.</p>
+                                        </div>
+                                        <div class="timeline-footer d-flex align-items-center flex-wrap">
+                                            <span class="ml-md-auto font-weight-bold">Septembre 2021</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="timeline-wrapper timeline-inverted timeline-wrapper-dev">
+                                    <div class="timeline-badge"></div>
+                                    <div class="timeline-panel">
+                                        <div class="timeline-heading">
+                                            <h6 class="timeline-title">La technologie DLSS plus facilement intégrable à Unity</h6>
+                                        </div>
+                                        <div class="timeline-body">
+                                            <p>Le DLSS est une technologie qui vise à calculer l’image en basse définition, pour l’afficher en haute résolution, ce qui permet d’augmenter les performances de notre carte graphique. On obtient un jeu à la fois fluide et beau. Hélas cette technologie est une propriété d’Nvidia. Mais celle-ci a annoncé le 14 avril 2021 que le moteur graphique intégrait à son tour le DLSS nativement.</p>
+                                        </div>
+                                        <div class="timeline-footer d-flex align-items-center flex-wrap">
+                                            <i class="mdi mdi-heart-outline text-muted mr-1"></i>
+                                            <span><a href="https://www.frandroid.com/marques/nvidia/909641_dlss-sur-unity-nvidia-prend-un-peu-plus-davance-avec-ladoption-du-moteur-tres-populaire">Source</a></span>
+                                            <span class="ml-md-auto font-weight-bold">Septembre 2021</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Fin timeline -->
+                <h1 class="jumbotron-heading">Comment se passe ma veille ?</h1>
+                    <p class="lead text-muted">
+                        Pour l'instant je fais des recherches sur des sujets qui m'intéressent ou bien j'écoute des recommandations.
+                    </p>
+            </section>
+            </div>
+        </div>
--- vues/template/navbar.php
+++ vues/template/navbar.php
@@ -2,8 +2,8 @@
                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar" aria-controls="navbar">
                     <span class="sr-only">Navigation raccourcie</span>☰
                 </button>
-                <a class="navbar-brand" href="#">Mon portfolio</a>
-                <div id="navbar" class="navbar-collapse collapse">
+                <a class="navbar-brand" href="#">Clément LOZE</a>
+                <div id="navbar" class="flex-column navbar-collapse collapse">
                     <ul class = "nav navbar-nav">
                         <li class="nav-item <?php if($page===0) { ?>active <?php } ?>">
                             <a href="<?php __DIR__?>/" class="nav-link">
@@ -23,25 +23,29 @@
                                 <li role="presentation" class="dropdown-header dropdown-item"> En autonomie </li>
                                  <li role="presentation" class="dropdown-item">
                                     <a role="menuitem" href="../../pages/C_portfolio.php">Portfolio</a>
-                                </li>
-                                <li role="presentation" class="dropdown-item">
-                                    <a role="menuitem" href="../../pages/C_seeulater.php">Prochain projet</a>
-                                </li>
-                                <li role="presentation" class="dropdown-item">
-                                    <a role="menuitem" href="../../pages/C_seeulater.php">Projet d'après</a>
-                                </li>
+                                 </li>
+                                <li role="presentation" class="dropdown-divider"></li>
+                                <li role="presentation" class="dropdown-header dropdown-item"> En classe </li>
+                                 <li role="presentation" class="dropdown-item">
+                                    <a role="menuitem" href="../../pages/C_seeulater.php"> Projet Nolark </a>
+                                 </li>
                                 <li role="presentation" class="dropdown-divider"></li>
                                 <li role="presentation" class="dropdown-header dropdown-item">
-                                    En entreprise</li>
-                                <li role="presentation" class="dropdown-item">
-                                    <a role="menuitem" href="../../pages/C_seeulater.php">Projet de 1ère année</a>
+                                    Stages</li>
+                                <li role="presentation" class="dropdown-item disabled">
+                                    <a role="menuitem" href="../../pages/C_seeulater.php">Stage de 1<sup>ère</sup> année</a>
                                 </li>
-                                <li role="presentation" class="dropdown-item">
-                                    <a role="menuitem" href="../../pages/C_seeulater.php">Projet de 2ème année</a>
+                                <li role="presentation" class="dropdown-item disabled">
+                                    <a role="menuitem" href="../../pages/C_seeulater.php">Stage de 2<sup>ème</sup> année</a>
                                 </li>
                             </ul>
                         </li>
-                        <li class="dropdown nav-item <?php if($page===3) { ?>active <?php } ?>">
+                        <li class="nav-item <?php if($page===3) { ?>active <?php } ?>">
+                            <a href="../../Pages/C_veille.php" class="nav-link">
+                                <span class ="fa fa-gears"></span> Veille technologique
+                            </a>
+                        </li>
+                        <li class="dropdown nav-item <?php if($page===4) { ?>active <?php } ?>">
                             <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button">
                                 <span class="fa fa-graduation-cap"></span> Jury E4 BTS SIO
                                 <span class="caret"></span>
@@ -51,42 +55,42 @@
                                 <li role="presentation" class="dropdown-item">
                                     <a role="menuitem" href="../../pages/C_Presentation_epreuve_E4.php">Présentation</a>
                                 </li>
-                                <li role="presentation" class="dropdown-item">
+                                <li role="presentation" class="dropdown-item disabled">
                                     <a role="menuitem" href="../../pages/C_seeulater.php">Application</a>
                                 </li>
-                                <li role="presentation" class="dropdown-item">
+                                <li role="presentation" class="dropdown-item disabled">
                                     <a role="menuitem" href="../../pages/C_seeulater.php">Missions à réaliser</a>
                                 </li>
-                                <li role="presentation" class="dropdown-item">
+                                <li role="presentation" class="dropdown-item disabled">
                                     <a role="menuitem" href="../../pages/C_seeulater.php">Annexes</a>
                                 </li>
                                 <li role="presentation" class="dropdown-divider"></li>
                                 <li role="presentation" class="dropdown-header dropdown-item"> Fiches d'examen </li>
-                                <li role="presentation" class="dropdown-item">
+                                <li role="presentation" class="dropdown-item disabled">
                                     <a role="menuitem" href="../../pages/C_seeulater.php">Situation professionnelle n°1</a>
                                 </li>
-                                <li role="presentation" class="dropdown-item">
+                                <li role="presentation" class="dropdown-item disabled">
                                     <a role="menuitem" href="../../pages/C_seeulater.php">Situation professionnelle n°2</a>
                                 </li>
                                 <li role="presentation" class="dropdown-divider"></li>
                                 <li role="presentation" class="dropdown-header dropdown-item"> Démonstrations </li>
-                                <li role="presentation" class="dropdown-item">
+                                <li role="presentation" class="dropdown-item disabled">
                                     <a role="menuitem" href="../../pages/C_seeulater.php">Démo SP n°1</a>
                                 </li>
-                                <li role="presentation" class="dropdown-item">
+                                <li role="presentation" class="dropdown-item disabled">
                                     <a role="menuitem" href="../../pages/C_seeulater.php">Démo SP n°2</a>
                                 </li>
                                 <li role="presentation" class="dropdown-divider"></li>
                                 <li role="presentation" class="dropdown-header dropdown-item"> Codes sources </li>
-                                <li role="presentation" class="dropdown-item">
+                                <li role="presentation" class="dropdown-item disabled">
                                     <a role="menuitem" href="../../pages/C_seeulater.php">Sources SP n°1</a>
                                 </li>
-                                <li role="presentation" class="dropdown-item">
+                                <li role="presentation" class="dropdown-item disabled">
                                     <a role="menuitem" href="../../pages/C_seeulater.php">Sources SP n°2</a>
                                 </li>
                             </ul>
-                        <li class ="nav-item <?php if($page===4) { ?>active <?php } ?>">
-                            <a href="../../pages/C_seeulater.php" class="nav-link"><span class="fa fa-envelope"></span>
+                        <li class ="nav-item <?php if($page===5) { ?>active <?php } ?>">
+                            <a href="../../pages/C_seeulater.php" class="nav-link disabled"><span class="fa fa-envelope"></span>
                                 Me contacter
                             </a>
                         </li> 
