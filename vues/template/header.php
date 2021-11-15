ce10d8ae30f2bddb0915860f6104b0d6ca03a9c5
Clément <clementloze@hotmail.fr>
2021-11-15 19:29:42 +0100

    Passage 2021 titre

--- vues/template/header.php
+++ vues/template/header.php
@@ -11,7 +11,7 @@
         <link href ="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
         <link rel="stylesheet" href="../../css/portfolio.css">
         <link rel="icon" href="<?php __DIR__ ?>/favicon.ico">
-        <title>Portfolio de Clément LOZE (BTS SIO 2020)</title>
+        <title>Portfolio de Clément LOZE (BTS SIO 2021)</title>
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
