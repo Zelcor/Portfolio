fa1ba1ec16af223cbed7616662ca5aabe02f81fc
Zelcor <clementloze@hotmail.fr>
2022-03-15 23:22:47 +0100

    ptit fix pour la navbar (active)

--- pages/C_404.php
+++ pages/C_404.php
@@ -2,7 +2,7 @@
 require_once(__DIR__ . '/../vues/template/header.php');
 ?>
     <link rel="stylesheet" href="/../css/realisations.css">
-<?php $page=5;
+<?php $page=null;
 require_once(__DIR__ . '/../vues/template/navbar.php');
 require_once(__DIR__ . '/../vues/V_404.php');
 require_once(__DIR__ . '/../vues/template/footer.php'); ?>
