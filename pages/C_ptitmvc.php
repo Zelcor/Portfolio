0f94030fb4ce026218ca8b314510ca0b984ce431
Zelcor <clementloze@hotmail.fr>
2022-03-15 23:15:08 +0100

    ajout page ptitmvc

---
+++ pages/C_ptitmvc.php
@@ -0,0 +1,9 @@
+<?php
+require_once(__DIR__ . '/../vues/template/header.php');
+?>
+<link rel="stylesheet" href="../css/realisations.css">
+<?php $page=2;
+    require_once(__DIR__ . '/../vues/template/navbar.php');
+    require_once(__DIR__ . '/../vues/V_ptitmvc.php');
+    require_once(__DIR__ . '/../vues/template/footer.php');
+?>
---
+++ vues/V_ptitmvc.php
@@ -0,0 +1,52 @@
+    <div class="row center">
+        <div class="jumbotron" id="jumbotron">
+            <section class="jumbotron text-center center shadow">
+                <div class="container">
+                    <h1 class="jumbotron-heading">TP MonPetitMVC</h1>
+                    <p class="lead text-muted">
+                        Cette activité m'a permis de mieux concevoir l'architecture MVC, de plus j'ai découvert l'Ajax !
+                    </p>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="card bg-light mb-4 shadow">
+                                <p class="card-header">Liste des clients présent dans la Base De Données</p>
+                                <div class="card-body">
+                                    <img class="card-img-top" src="../image/ptit-mvc-liste-cli.png" alt="Liste clients">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card bg-light mb-4 shadow">
+                                <p class="card-header">Résultat affiché lors du clic sur le bouton de l'image précédente</p>
+                                <div class="card-body">
+                                    <img class="card-img-top" src="../image/clic-btn-voir_comm_cli2.png" alt="détail client + liste des commandes">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card bg-light mb-4 shadow">
+                                <p class="card-header">Voir un client et l'éditer en Ajax (sans recharger de page)</p>
+                                <div class="card-body">
+                                    <img class="card-img-top" src="../image/ptit-mvc-ajax.png" alt="Recherche client ajax">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card bg-light mb-4 shadow">
+                                <p class="card-header">Formulaire dynamique en fonction de la bdd</p>
+                                <div class="card-body">
+                                    <img class="card-img-top" src="../image/recherche-cli-depuis-bdd.png" alt="Formulaire de recherche">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-between">
+                    <a role="button" class="btn btn-primary" href="../pages/C_choco.php">↢ TP Chocolate'in</a>
+                    <a role="button" class="btn btn-primary" href="#">Projet GSB (bientôt) ↣</a>
+                </div>
+            </section>
+        </div>
+    </div>
