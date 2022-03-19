dfb0e95de372c1e74ea2f6e178669296e30d0cb0
Zelcor <clementloze@hotmail.fr>
2022-03-20 00:50:58 +0100

    avancement projet 1 GSB

--- vues/V_Projet_1_GSB.php
+++ vues/V_Projet_1_GSB.php
@@ -3,69 +3,44 @@
         <section class="jumbotron text-center">
             <div class="container">
                 <h1 class="jumbotron-heading "><u>Projet 1 : Galaxy Swiss Bourdin</u></h1>
-                <img class="image-responsive" src="../image/DIRISI.png" alt="logo dirisi">
+                <img class="image-responsive" src="https://i.imgur.com/qdkr3AY.jpg" alt="logo gsb">
                 <p class="lead">
-                    Les technologies utilisées lors de ce stage furent : <strong>Angular</strong>, <strong>node.js</strong>, <strong>PostgreSQL</strong>
+                    Les technologies utilisées lors de ce projet furent : <strong>PHP 7</strong>, <strong>IDE NetBeans</strong>, <strong>MariaDB</strong>
                 </p>
-                <hr>
-                <h3 class="jumbotron-heading"><u> Présentation de ce stage : </u></h3>
-                <p class="lead">
-                    Mon stage s’est déroulé au fort Lamalgue, plus précisément au pôle développement de la <strong>DIRISI</strong>.
-                    A mon arrivée, l’équipe de 7 développeurs m’a informé que les technologies utilisées sont <strong>Node.js</strong> et <strong>Angular</strong>.
+                <h2 class="jumbotron-heading">Contexte du projet : </h2>
+                <p class="lead text-justify">
+                    Le laboratoire Galaxy Swiss Bourdin (GSB) est issu de la fusion entre le géant américain <strong>Galaxy</strong> (spécialisé dans le secteur des maladies virales dont le SIDA et les hépatites) et le conglomérat européen
+                    <strong>Swiss Bourdin</strong> (travaillant sur des médicaments plus conventionnels), lui-même déjà union de trois
+                    petits laboratoires.
+                    Après deux années de réorganisations internes, tant au niveau du personnel que du fonctionnement
+                    administratif, l'entreprise <strong>GSB souhaite moderniser</strong> l'activité de visite médicale.
+                    Une <strong>gestion</strong> unique de ces <strong>frais</strong> et <strong>remboursement</strong> pour l'ensemble de la flotte visite est souhaitée.
                 </p>
-                <div class="col-md-auto">
-                    <div class="card bg-light mb-4 shadow">
-                        <p class="card-header">Preuve autoformation open-classroom</p>
-                        <div class="card-body colapse multi-collapse hide" data-toggle="collapse" data-target="#auto-formation" aria-controls="auto-formation" aria-expanded="true">
-                            <small>
-                                cliquez sur l'image pour afficher le texte explicatif
-                                <img class="card-img-top" src="../image/openclassroom_autoformation_dirisi.jpg" alt="autoformation_dirisi">
-                            </small>
-                        </div>
+                <div class="text-left">
+                    <h3 class="jumbotron-heading text-left colapse multi-collapse" data-toggle="collapse" data-target="#generation_pdf" aria-controls="generation_pdf" aria-expanded="true">Tâche 1 : Génération d'un pdf</h3>
+                    <hr>
+                    <div class="collapse multi-collapse show" id="generation_pdf">
+                        <p>blabla</p>
+                    </div>
+                    <h3 class="jumbotron-heading text-left colapse multi-collapse" data-toggle="collapse" data-target="#eco_dans_appli" aria-controls="eco_dans_appli" aria-expanded="true">Tâche 2 : Davantage d'écologie dans l'application</h3>
+                    <hr>
+                    <div class="collapse multi-collapse show" id="eco_dans_appli">
+                        <p>blabla</p>
+                    </div>
+                    <h3 class="jumbotron-heading text-left colapse multi-collapse" data-toggle="collapse" data-target="#doc_technique_pdf" aria-controls="doc_technique_pdf" aria-expanded="true">Tâche 3 : Documentation technique pour le PDF</h3>
+                    <hr>
+                    <div class="collapse multi-collapse show" id="doc_technique_pdf">
+                        <p>blabla</p>
                     </div>
                 </div>
-                <div class="collapse multi-collapse" id="auto-formation">
-                    <p class="lead">
-                        Ne maîtrisant ni l’une ni l'autre, j’ai dû me former sur OpenClassrooms (comme la plupart des développeurs présents).
-                        Ces formations m’ont pris l’entièreté de la 1ère semaine de mon stage.
-                        J’ai dû m’arrêter à la moitié parce qu’il y avait un problème de version entre la version utilisée par la formation et ma version locale.
-                    </p>
+                <h2 class="jumbotron-heading">Conclusion : </h2>
+                <p class="lead">
+                    texte de conclu
+                </p>
+                <div class="d-flex justify-content-between">
+                    <a role="button" class="btn btn-primary" href="../pages/C_ptitmvc.php">↢ TP : MonPetitMVC</a>
+                    <a role="button" class="btn btn-primary" href="#">Projet 2 : MDL ↣</a>
                 </div>
-
-                <p class="lead">
-                    Une fois ces technologies assimilées j’ai été ajouté au GitLab du projet sur lequel travaille l’équipe.
-                    Le projet s’appelle <strong>SIRA</strong> et il vise à fournir un service de santé permettant de surveiller l’exposition des militaires à la radiation atomique.
-                    Au sein de ce projet, nous devions faire fonctionner les applications avec <strong>Docker</strong> et <strong>Angular</strong>.
-                </p>
-                <div class="col-md-auto">
-                    <div class="card bg-light mb-4 shadow">
-                        <p class="card-header">Page Accueil SIRA</p>
-                        <div class="card-body" data-toggle="collapse" data-target="#auto-formation" aria-controls="auto-formation" aria-expanded="true">
-                            <img class="card-img-top" src="../image/sira.png" alt="accueil Sira">
-                        </div>
-                    </div>
-                </div>
-                <p class="lead">
-                    Par la suite, j'ai fait de <strong>multiples écrans graphiques</strong> pour l’application afin de me familiariser avec <strong>Angular</strong>.
-                    Pour chaque nouvel écran je devais l'implémenter dans la navbar par le biais de la Base De Données.
-                    De plus il fallait s’occuper du routage afin que l’utilisateur puisse sélectionner le nouvel écran dans la barre de navigation et qu'il soit dirigé vers la bonne page, il en est de même pour tous les boutons utilisés.
-                    Mais aussi, chaque fenêtre doit pouvoir afficher et une fois validés ajoutés dans la BDD
-                    (dans une fenêtre où il fallait que je m’occupe de motifs je devais à la fois les afficher depuis la base et pouvoir en ajouter dans la base contenant tous les motifs).
-                </p>
-                <p class="lead">
-                    J’ai par la suite essayé d’organiser une liste de rattachements avec un rapport parent-enfant (par exemple dans les sous-marins il y a la sous-catégorie des SNLE).
-                    Cette partie a mis à l’épreuve mon <strong>raisonnement algorithmique</strong> avec moult solutions apportées par l’équipe de développement.
-                </p>
-                <hr>
-                <h3 class="jumbotron-heading"><u>Conclusion sur ce stage : </u></h3>
-                <p class="lead">
-                    Ce stage m’a permis de découvrir le monde de l’entreprise, de plus il m’a apporté des connaissances en Node.js et Angular, deux technologies recherchées par les employeurs.
-                    Je souhaiterai approfondir mes connaissances dans ses technologies ou bien en découvrir d’autres dans mon prochain stage.
-                </p>
-                <hr>
-                <h3 class="jumbotron-heading"><u> Télécharger ma synthèse de stage : </u></h3>
-                <p class="lead text-muted">Vous pouvez retrouver ci-après le lien menant vers ma synthèse de stage</p>
-                <a role="button" class="btn btn-primary mb-2 p-2" href="../image/documents/Synthèse%20de%20stage.pdf">Télécharger ma synthèse de stage</a>
             </div>
         </section>
     </div>
