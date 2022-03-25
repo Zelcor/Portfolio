e24056356279d1708de09efb4175256e8f34f350
Zelcor <clementloze@hotmail.fr>
2022-03-25 17:47:45 +0100

    optimisation projet

--- vues/V_seeulater.php
+++
@@ -1,8 +0,0 @@
-        <div class="jumbotron" id="jumbotron">
-            <div class="cadreCV">
-            <div class="css/portfolio.css">
-                <img src="../image/en%20travaux.jpg" class="rounded mx-auto d-block">
-                <h1 class="d-flex justify-content-center"> Ce site n'est pas fini, mais il vient de passer en php !! son dévelloppement va être bien plus rapide tout de suite :o</h1>
-            </div>
-            </div>
-        </div> <!-- Emplacement pour le contenu principal de la page -->
