cd8bb2cc962bf7ee87b205e491a668e559778875
Clément <clementloze@hotmail.fr>
2022-03-30 20:04:13 +0200

    Auto stash before merge of "develop" and "origin/develop"

---
+++ vues/V_Chocolatin.php
@@ -0,0 +1,40 @@
+    <div class="jumbotron" id="jumbotron">
+        <div class="row center">
+            <section class="jumbotron text-center">
+                <div class="container">
+                    <h1 class="jumbotron-heading">Projet Chocolate'in</h1>
+                    <p class="lead text-muted">
+                        Cette activité m'a permis de m'initier au développement web en HTML5/CSS3/JS, notamment à l'utilisation de bootstrap. Ce fut très instructif. <br> Cette activité nous a aussi sensibilisé au RGPD.
+                    </p>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card mb-4 shadow-sm">
+                                <img class="card-img-top" src="../image/nolark_accueil.jpg" alt="accueil nolark">
+                                <div class="card-body">
+                                    <p class="card-text">Page d'accueil</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card mb-4 shadow-sm">
+                                <img class="card-img-top" src="../image/nolark_route.jpg" alt="page exposition casque route nolark">
+                                <div class="card-body">
+                                    <p class="card-text">Liste de casques depuis la base de donnée en fonction de leurs caractéristiques</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card mb-4 shadow-sm">
+                                <img class="card-img-top" src="../image/nolark_contact.jpg" alt="page contact nolark">
+                                <div class="card-body">
+                                    <p class="card-text">Page de contact</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </section>
+            </div>
+        </div>
